20cca1165901b18e5cf4daa605500a484993395a
9OAT0 <oathaha30@gmail.com>
2025-03-05 23:52:20 +0700

    Update page.tsx

--- app/(website)/dashBord/page.tsx
+++ app/(website)/dashBord/page.tsx
@@ -159,6 +159,7 @@ const Dashboard: React.FC = () => {
       alert("✅ อัปเดตคำสั่งซื้อสำเร็จ!");
       setEditOrder(null);
       fetchOrders();
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
     } catch (error: any) {
       console.error("❌ Error updating order:", error);
       alert(
